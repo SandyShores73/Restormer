ec0a70c39d853977d18805d3441b4078f74f23e3
SandyShores73 <eli.joseph.shipley@gmail.com>
2025-10-10 13:12:56 -0500

    Update .eslintrc.cjs

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,3 +1,4 @@
+ (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
 diff --git a//dev/null b/client/.eslintrc.cjs
 index 0000000000000000000000000000000000000000..97dd3a7617c4c367aca9841903294f72b9593414 100644
 --- a//dev/null
@@ -22,3 +23,6 @@ index 0000000000000000000000000000000000000000..97dd3a7617c4c367aca9841903294f72
 +    }
 +  }
 +};
+ 
+EOF
+)
